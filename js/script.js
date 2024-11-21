6e21b6435cbbf838fd30178ae9b53d414a769a0a
LincolnBNeto <lincoln.brunneto@gmail.com>
2024-11-21 16:28:40 -0300

    Criei mais uma página, ainda faltam duas

--- js/script.js
+++ js/script.js
@@ -9,3 +9,14 @@ botaoHamburguer.addEventListener("click", function () {
 botaoFechar.addEventListener("click", function () {
   menuCelular.classList.remove("flex");
 });
+
+function validateEmail(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+}
+
+function validateTelefone(telefone) {
+  const re = /^\(\d{2}\) \d{5}-\d{4}$/;
+  return re.test(telefone);
+}
+
